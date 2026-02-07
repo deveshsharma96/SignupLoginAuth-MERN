a3e09580e79894671eeddf2ec58219e57bc1ab04
Devesh Sharma <deveshsharma9958@gmail.com>
2026-02-07 20:08:46 +0530

    Fix URL by removing duplicate slash

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
                 return handleError('email and password are required')
             }
             try{
-                const url = "https://registration-login-authentication-m.vercel.app//auth/login";
+                const url = "https://registration-login-authentication-m.vercel.app/auth/login";
                 const response = await fetch(url, {
                     method: 'POST',
                     headers: {
