343b0f1b90b25ab23fc50acb0f83956f6824037c
Devesh Sharma <deveshsharma9958@gmail.com>
2026-02-07 20:07:24 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -31,7 +31,7 @@ function Signup() {
                 return handleError('name, email and password are required')
             }
             try{
-                const url = "http://localhost:8080/auth/signup";
+                const url = "https://registration-login-authentication-m.vercel.app/auth/signup";
                 const response = await fetch(url, {
                     method: 'POST',
                     headers: {
